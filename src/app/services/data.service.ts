8d34fbe6483c701d35ffc52603e0168aebe9fd45
Gerald Tagle <78480062+devTags@users.noreply.github.com>
2022-09-13 16:29:52 +0800

    Update data.service.ts

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,33 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { UserTable } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  baseUrl: string = 'https://631ee3db22cefb1edc3ced68.mockapi.io/'
+  baseUrl: string = 'https://631ee3db22cefb1edc3ced68.mockapi.io/';
 
   constructor(private _http: HttpClient) { }
 
 
 
-  public get(endpoint: string){
-    return this._http.get(this.baseUrl + endpoint)
+  public  get(endpoint: string){
+    return this._http.get<UserTable[]>(this.baseUrl + endpoint);
 
   }
 
-  public post(endpoint: string,jsonData: any) {
-    return this._http.post(this.baseUrl + endpoint, jsonData)
+  public post(endpoint: string, userData: UserTable[]){
+    return this._http.post<UserTable[]>(this.baseUrl + endpoint, userData)
   }
 
-  public put(endpoint: string, id: number, jsonData: any) {
-    return this._http.put(this.baseUrl + endpoint +'/'+id, jsonData)
+  public put(endpoint: string, id: number, userData: UserTable[]) {
+    return this._http.put<UserTable[]>(`${this.baseUrl}${endpoint}/${id}`, userData)
 
   }
 
   public delete(endpoint: string){
-    return this._http.delete(this.baseUrl + endpoint)
+    return this._http.delete<UserTable[]>(this.baseUrl + endpoint)
   }
 
 }
