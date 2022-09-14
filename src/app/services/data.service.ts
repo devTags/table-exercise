2d54ea456bfd5b555348565d3aa6d02363b75653
Gerald Tagle <78480062+devTags@users.noreply.github.com>
2022-09-14 16:41:34 +0800

    Update data.service.ts

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { UserTable } from '../interfaces';
 
@@ -14,22 +15,22 @@ export class DataService {
 
 
 
-  public  getUsers(){
+  public  getUsers(): Observable<UserTable[]>{
     return this._http.get<UserTable[]>(this.baseUrl + 'getDataTable' );
 
   }
 
-  public addUsers(userData: UserTable[]){
+  public addUsers(userData: UserTable[]): Observable<UserTable[]>{
     return this._http.post<UserTable[]>(this.baseUrl + 'getDataTable', userData)
   }
 
-  public putUsers(id: number, userData: UserTable[]) {
-    return this._http.put<UserTable[]>(`${this.baseUrl}/getDataTable/${id}`, userData)
+  // public putUsers(id: number, userData: UserTable[]): Observable<UserTable[]>{
+  //   return this._http.put<UserTable[]>(`${this.baseUrl}/getDataTable/${id}`, userData)
 
-  }
+  // }
 
-  public deleteUsers(id: number){
-    return this._http.delete<UserTable[]>(`${this.baseUrl}/${id}`)
-  }
+  // public deleteUsers(id: number): Observable<UserTable[]>{
+  //   return this._http.delete<UserTable[]>(`${this.baseUrl}/getDataTable/${id}`)
+  // }
 
 }
