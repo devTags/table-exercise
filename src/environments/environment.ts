08e3601ccba84d089000f25c759c937a342ec8aa
Gerald Tagle <78480062+devTags@users.noreply.github.com>
2022-09-14 11:24:53 +0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl: 'https://631ee3db22cefb1edc3ced68.mockapi.io/'
 };
 
 /*
